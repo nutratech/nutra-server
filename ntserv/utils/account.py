0eec311cbeabe0a9391373ab5c5ffc34b3e01673
gamesguru <mathmuncher11@gmail.com>
2020-01-28 17:15:21 -0500

    fix prod error on account.py

--- ntserv/utils/account.py
+++ ntserv/utils/account.py
@@ -6,7 +6,7 @@ def user_id_from_username(username):
     pg_result = psql("SELECT id FROM users WHERE username=%s", [username])
 
     # ERRORs
-    if pg_result.err_msg:
+    if pg_result.err_msg or not pg_result.rows:
         return None
 
     return pg_result.row["id"]
