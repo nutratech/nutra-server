0ec3b32e2e96e6298f9d07f7eb37cc9c4040f7fb
gamesguru <mathmuncher11@gmail.com>
2020-02-02 21:15:07 -0500

    add cronjob caffeinate()

--- ntserv/utils/caffeine.py
+++ ntserv/utils/caffeine.py
@@ -1,3 +1,13 @@
+from crontab import CronTab
+
+from ..settings import SERVER_HOST
+
+
 def caffeinate():
-    # TODO - cron job
-    return None
+    return
+    # Keeps server awake
+    cron = CronTab()
+    job = cron.new(command=f"/usr/bin/curl {SERVER_HOST}")
+    # At minute 0 and 30 past every hour from 6 through 20.
+    job.setall("00,30 06-20 * * *")
+    job.run()
