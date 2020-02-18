ce3fc4c7e296cc9a01f1f548c894f96a315bf1c8
gamesguru <mathmuncher11@gmail.com>
2020-02-18 09:55:19 -0500

    try removing pytest from requirements.txt

--- test_ntserv.py
+++ test_ntserv.py
@@ -24,9 +24,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+import json
 import pytest
 from fuzzywuzzy import fuzz
-import ujson as json
 
 from ntserv.postgres import psql
 
