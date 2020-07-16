70f51fbf6d44f5907185aac5a54e753369f8e02d
gamesguru <mathmuncher11@gmail.com>
2020-07-16 10:55:32 -0400

    fix casey butt according to:
    - http://www.weightrainer.net/potential.html

--- ntserv/usda.py
+++ ntserv/usda.py
@@ -40,28 +40,34 @@ def GET_calc_bodyfat(request):
         denom = (
             1.0324 - 0.19077 * math.log10(waist - neck) + 0.15456 * math.log10(height)
         )
-    else:
+    elif gender == "female":
         denom = (
             1.29579
             - 0.35004 * math.log10(waist + hip - neck)
             + 0.22100 * math.log10(height)
         )
+    else:
+        denom = 1
     navy = round(495 / denom - 450, 2)
 
     # 3-site test
     s3 = chest + ab + thigh
     if gender == "male":
         denom = 1.10938 - 0.0008267 * s3 + 0.0000016 * s3 * s3 - 0.0002574 * age
-    else:
+    elif gender == "female":
         denom = 1.089733 - 0.0009245 * s3 + 0.0000025 * s3 * s3 - 0.0000979 * age
+    else:
+        denom = 1
     three_site = round(495 / denom - 450, 2)
 
     # 7-site test
     s7 = chest + ab + thigh + tricep + sub + sup + mid
     if gender == "male":
         denom = 1.112 - 0.00043499 * s7 + 0.00000055 * s7 * s7 - 0.00028826 * age
-    else:
+    elif gender == "female":
         denom = 1.097 - 0.00046971 * s7 + 0.00000056 * s7 * s7 - 0.00012828 * age
+    else:
+        denom = 1
     seven_site = round(495 / denom - 450, 2)
 
     return Response(
@@ -92,21 +98,23 @@ def GET_calc_lblimits(request):
     h = height / 2.54
     w = wrist / 2.54
     a = ankle / 2.54
-    weight = round(
+    lbm = round(
         h ** (3 / 2)
         * (math.sqrt(w) / 22.6670 + math.sqrt(a) / 17.0104)
         * (1 + desired_bf / 224),
         1,
     )
+    weight = round(lbm / (1 - desired_bf / 100), 1)
     cb = {
         "notes": f"{desired_bf}% bodyfat",
+        "lbm": f"{lbm} lbs",
         "weight": f"{weight} lbs",
-        "chest": round(1.625 * w + 1.3682 * a + 0.3562 * h, 2),
-        "arm": round(1.1709 * w + 0.1350 * h, 2),
-        "forearm": round(0.950 * w + 0.1041 * h, 2),
-        "neck": round(1.1875 * w + 0.1301 * h, 2),
-        "thigh": round(1.4737 * a + 0.1918 * h, 2),
-        "calf": round(0.9812 * a + 0.1250 * h, 2),
+        "chest": round(1.6817 * w + 1.3759 * a + 0.3314 * h, 2),
+        "arm": round(1.2033 * w + 0.1236 * h, 2),
+        "forearm": round(0.9626 * w + 0.0989 * h, 2),
+        "neck": round(1.1424 * w + 0.1236 * h, 2),
+        "thigh": round(1.3868 * a + 0.1805 * h, 2),
+        "calf": round(0.9298 * a + 0.1210 * h, 2),
     }
     return Response(data={"martin-berkhan": mb, "eric-helms": eh, "casey-butt": cb})
 
