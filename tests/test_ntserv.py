6f28fd953ed5c80e44d6cf62482cbdee0757bc16
Shane Jaroch <chown_tee@proton.me>
2022-06-27 00:36:46 -0400

    remove refs to __release__ and nasty git parsing

--- tests/test_ntserv.py
+++ tests/test_ntserv.py
@@ -4,17 +4,8 @@ Created on Sat Feb  1 13:18:44 2020
 
 @author: shane
 """
-from datetime import datetime
-
 from ntserv.persistence.psql import verify_db_version_compat
-from ntserv.utils import release_git_parse
 
 
 def test_verify_db_version_compat():
     assert verify_db_version_compat()
-
-
-def test_release_git_parse():
-    _release = release_git_parse()
-    assert _release[0]
-    assert datetime.strptime(_release[1], "%Y-%m-%d %H:%M:%S %z")
