474940f24d3c1a02f5d17ac1c40ae8c743288521
Shane Jaroch <chown_tee@proton.me>
2022-07-17 09:55:10 -0400

    postgres not running for test, lower coverage

--- tests/test_ntserv.py
+++ tests/test_ntserv.py
@@ -4,6 +4,8 @@ Created on Sat Feb  1 13:18:44 2020
 
 @author: shane
 """
+import pytest
+
 from ntserv.persistence.psql import verify_db_version_compat
 
 
@@ -11,4 +13,4 @@ def test_verify_db_version_compat():
     try:
         assert verify_db_version_compat()
     except KeyError:
-        print("Skipping test... Postgres likely not running.")
+        pytest.skip("Skipping test... Postgres likely not running.")
